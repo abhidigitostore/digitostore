c8219ec8c8559cc2e28911beb55664777f7806a0
Sumanth-Siddareddy <siddareddy.venkatasumanth28@gmail.com>
2025-08-13 14:46:17 +0530

    Fix : Set the theme of the website to light always

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Navbar />
